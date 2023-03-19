b9f2e320b4c3e2e1138c0770a5bcd947ba31e8ab
Rakibul Hasan <rakib@Rakibuls-MacBook-Pro.local>
2023-03-19 01:04:48 -0400

    Start the ink

---
+++ source/cli.tsx
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './ui';
+
+const cli = meow(`
+	Usage
+	  $ find-pid-from-port
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ find-pid-from-port --name=Jane
+	  Hello, Jane
+`, {
+	flags: {
+		name: {
+			type: 'string'
+		}
+	}
+});
+
+render(<App name={cli.flags.name}/>);
