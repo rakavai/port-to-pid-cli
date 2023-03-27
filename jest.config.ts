850e033d6270853af604407a7e060630bcbf5f98
Rakibul Hasan <rakib@Rakibuls-MacBook-Pro.local>
2023-03-26 21:10:09 -0400

    Bootstrap working both test and the impl

--- jest.config.ts
+++ jest.config.ts
@@ -1,30 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	// preset: 'ts-jest',
-	// extensionsToTreatAsEsm: ['.ts', '.tsx'],
 	preset: 'ts-jest/presets/js-with-ts',
-	// preset: 'ts-jest/presets/default-esm',
 	testEnvironment: "node",
-	// "extensionsToTreatAsEsm": [".ts"],
-	// "transform": {
-	// 	"node_modules/variables/.+\\.(j|t)sx?$": ["ts-jest",'<rootDir>/tsconfig.test.json' ]
-	// },
-	globals: {
-		'ts-jest': {
-			tsconfig: '<rootDir>/tsconfig.test.json',
-		},
+	transform: {
+		'^.+\\.tsx?$': [
+			'ts-jest',
+			{tsconfig: '<rootDir>/tsconfig.test.json'},
+		],
 	},
 	transformIgnorePatterns: [
 		"node_modules/(?!ink-testing-library/.*)",
-	],
-
-	//---------------
-	// extensionsToTreatAsEsm: ['.ts'],
-	// verbose: true,
-	// preset: 'ts-jest/presets/default-esm',
-	// testEnvironment: 'node',
-	// transform: {
-	// 	'^.+\\.(ts|tsx)?$': ['ts-jest', { useESM: true }]
-	// },
-	// testPathIgnorePatterns: ['./dist']
+	]
 };
