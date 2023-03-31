5733b5420cec5923b412d18bfdf5bea745266b8e
Rakibul Hasan <rakibulh9@gmail.com>
2023-03-31 00:42:49 -0400

    The test works

--- source/test/app.test.tsx
+++ source/test/app.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import chalk from 'chalk';
-import {render} from 'ink-testing-library';
+const {render} = require('ink-testing-library');
 import App from '../app';
 
 
@@ -10,7 +10,7 @@ describe('app', function () {
             it('print out the port no', function () {
                 const {lastFrame} = render(<App port={"45066"}/>);
 
-                expect(lastFrame()).toEqual( chalk`Port {green.bold 45066} is being used by the following process:`);
+                expect(lastFrame()).toEqual( chalk`Port {bold.green 45066} is being used by the following process:`);
             });
         });
     });
